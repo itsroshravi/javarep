bad5dff26001acdbd69f649512ab40bc5436f7a8
Roshna Ravindran <112476732+itsroshravi@users.noreply.github.com>
2026-02-23 12:32:42 +0530

    Update output message in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("New Hello World!");
+        System.out.println("New Hello World1!");
         System.out.println("Addition: " + (2 + 3));
     }
 }
+
 
