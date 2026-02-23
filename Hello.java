f0557eb86235db550aaa7f53f5d0fdd6854eb0a1
Roshna Ravindran <112476732+itsroshravi@users.noreply.github.com>
2026-02-23 14:04:28 +0530

    Refactor Hello class and update output messages

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello3 World!");
-        System.out.println("New Hello World1!");
+        System.out.println("Hello World!");
         System.out.println("Addition: " + (2 + 3));
     }
 }
+
 
 
 
