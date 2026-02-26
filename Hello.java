f2c105060f451f827082c6b863fe779be76cf7af
Roshna Ravindran <112476732+itsroshravi@users.noreply.github.com>
2026-02-26 14:29:12 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,9 +2,10 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
               
-        System.out.println("Addition: " + (2 + 3));
+        System.out.println("Addition: " + (2 + 10));
     }
 }
+
 
 
 
